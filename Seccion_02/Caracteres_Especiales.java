aa0f488d30c3a9f5184245f434d9e989c7e72fe0
alexandercooper97 <130676645+alexandercooper97@users.noreply.github.com>
2023-04-29 08:32:22 -0500

    Add files via upload

---
+++ Seccion_02/Caracteres_Especiales.java
@@ -0,0 +1,15 @@
+//package Seccion_02;
+
+public class Caracteres_Especiales {
+    public static void main(String args[]){
+        var nombre = "Karla";
+
+        System.out.println("Nueva linea : \n" + nombre);
+        System.out.println("Tabulador : \t" + nombre);
+        System.out.println("Retroceso : \b\b" + nombre);
+        System.out.println("Comilla Simple : \'" + nombre + "\' ");
+        System.out.println("Comilla Simple : '" + nombre + "' ");
+        System.out.println("Comilla Doblwe : \"" + nombre + "\" ");
+    }
+}
+
